e76fd19c95bf4fdda758691d149cb0372e8a32b0
Martin Galese <martin@galese.net>
2024-03-24 15:58:07 -0400

    add bug fix to PADUS

--- publiclands/merge_data.py
+++ publiclands/merge_data.py
@@ -5,9 +5,13 @@ import pandas as pd
 # %%
 # Load the data
 nh = gpd.read_file("../data/public_lands_nh.gpkg").to_crs("EPSG:4326")
+# %%
 ma = gpd.read_file("../data/public_lands_ma.gpkg").to_crs("EPSG:4326")
+# %%
 me = gpd.read_file("../data/public_lands_me.gpkg").to_crs("EPSG:4326")
+# %%
 ri = gpd.read_file("../data/public_lands_ri.gpkg").to_crs("EPSG:4326")
+# %%
 padus = gpd.read_file("../data/public_lands_padus.gpkg").to_crs("EPSG:4326")
 
 # %%
@@ -16,3 +20,5 @@ public_lands = gpd.GeoDataFrame(pd.concat([nh, ma, me, ri, padus], ignore_index=
 
 # %%
 public_lands.to_file("../data/public_lands.gpkg", driver="GPKG")
+
+# %%
